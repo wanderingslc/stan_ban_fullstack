12bbf743460e136b45c6aa2155ee8506cb2d0194
wanderingslc <stan.martineau@gmail.com>
2018-11-23 11:53:31 -0700

    Work on initial project object

---
+++ stan_ban/src/main/java/com/stanban/ppmtool/Project.java
@@ -0,0 +1,24 @@
+package com.stanban.ppmtool;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
+public class Project {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String projectName;
+    private String projectIdentifier;
+    private String description;
+    private Date start_date;
+    private Date end_date;
+
+    private Date created_at;
+    private Date update_at;
+
+}
